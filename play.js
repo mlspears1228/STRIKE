0c571ae92e86e72320eb4facedf967ef1b22bfd3
Marlin <mlspears1228@vt.edu>
2023-12-01 16:05:10 -0500

    Player States

--- play.js
+++ play.js
@@ -4,18 +4,19 @@ class Play {
         this.isPaused = false;
         this.testTilemap = new Tilemap();
         loadXML("assets/tilemaps/test.tmx", (xml) =>
-            loadTilemap(xml, this.testTilemap),
+            loadTilemap(xml, this.testTilemap)
         );
-        this.player = new Player(0, 144);
+        this.player = new Player(32, 160);
     }
 
     loop() {
         // Draw the game.
         push();
-        scale(4);
-        translate(0, -144);
+        // scale(4);
+        // translate(0, -144);
         background(220);
         renderTilemap(this.testTilemap);
+        this.player.draw();
         pop();
         // Pause is drawn on top of the game.
         // if (isPaused) {}
--- player.js
+++ player.js
@@ -1,6 +1,88 @@
+const PlayerStates = {
+    IDLE: "idle",
+    WALKING: "walking",
+    JUMPING: "jumping",
+    RUNNING: "running",
+};
 class Player {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.idleAnimation = [];
+        this.idleAnimation.push(
+            loadImage(
+                "assets/player animations/Character Idle/images/Character-Idle-48x48_01.png"
+            )
+        );
+        this.idleAnimation.push(
+            loadImage(
+                "assets/player animations/Character Idle/images/Character-Idle-48x48_02.png"
+            )
+        );
+        this.idleAnimation.push(
+            loadImage(
+                "assets/player animations/Character Idle/images/Character-Idle-48x48_03.png"
+            )
+        );
+        this.idleAnimation.push(
+            loadImage(
+                "assets/player animations/Character Idle/images/Character-Idle-48x48_04.png"
+            )
+        );
+        this.idleAnimation.push(
+            loadImage(
+                "assets/player animations/Character Idle/images/Character-Idle-48x48_05.png"
+            )
+        );
+        this.idleAnimation.push(
+            loadImage(
+                "assets/player animations/Character Idle/images/Character-Idle-48x48_06.png"
+            )
+        );
+        this.idleAnimation.push(
+            loadImage(
+                "assets/player animations/Character Idle/images/Character-Idle-48x48_07.png"
+            )
+        );
+        this.idleAnimation.push(
+            loadImage(
+                "assets/player animations/Character Idle/images/Character-Idle-48x48_08.png"
+            )
+        );
+        this.idleAnimation.push(
+            loadImage(
+                "assets/player animations/Character Idle/images/Character-Idle-48x48_09.png"
+            )
+        );
+        this.idleAnimation.push(
+            loadImage(
+                "assets/player animations/Character Idle/images/Character-Idle-48x48_10.png"
+            )
+        );
+        this.playerState = PlayerStates.IDLE;
+        this.currentFrame = 0;
+    }
+
+    draw() {
+        push();
+        imageMode(CENTER);
+        scale(1 / 3);
+        switch (this.playerState) {
+            case PlayerStates.IDLE:
+                this.drawIdle();
+                break;
+            case PlayerStates.WALKING:
+                this.drawWalking();
+                break;
+            case PlayerStates.JUMPING:
+                this.drawJumping();
+                break;
+        }
+        pop();
+    }
+
+    drawIdle() {
+        image(this.idleAnimation[this.currentFrame], this.x, this.y);
+        this.currentFrame += this.currentFrame % this.idleAnimation.length;
     }
 }
